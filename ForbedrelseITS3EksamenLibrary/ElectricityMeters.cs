606386fbf55cc6d5cfff44532395d29b9899c54b
Cath20201 <78859065+Cath20201@users.noreply.github.com>
2023-02-22 16:34:00 +0100

    metod random

--- ForbedrelseITS3EksamenLibrary/ElectricityMeters.cs
+++ ForbedrelseITS3EksamenLibrary/ElectricityMeters.cs
@@ -2,15 +2,23 @@
 {
     public class ElectricityMeters : IElectricityMeters
     {
+        private readonly Random random = new Random();
+        private int randomNb;
         public int ID { get; set; }
         public double Forbrug { get; set; }
         public DateTime Time { get; set; }
+        
 
         public ElectricityMeters(int iD, double forbrug, DateTime time)
         {
             ID = iD;
             Forbrug = forbrug;
             Time = time;
+        }
+        public double GetElconsumption()
+        {
+            randomNb = random.Next(5, 40);
+            return randomNb / 100.0;
         }
     }
 }
