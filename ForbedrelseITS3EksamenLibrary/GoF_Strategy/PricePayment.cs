b6abb4eee98def91193e7fdc18ae3b1a60eb319e
Cath20201 <78859065+Cath20201@users.noreply.github.com>
2023-02-23 17:49:47 +0100

    .

--- ForbedrelseITS3EksamenLibrary/GoF_Strategy/PricePayment.cs
+++ ForbedrelseITS3EksamenLibrary/GoF_Strategy/PricePayment.cs
@@ -9,7 +9,16 @@ namespace ForbedrelseITS3EksamenLibrary.GoF_Strategy
 {
     public class PricePayment : IDataObserver
     {
+        private readonly MeterdataMontior _meterdataMontior;
+        public IExpenseForPayment PricePaymentType { get; set; }
 
+        public PricePayment(MeterdataMontior meterdataMontior)
+        {
+            meterdataMontior.Attach(this);
+            _meterdataMontior = meterdataMontior;
+
+
+        }
         public void Update()
         {
             throw new NotImplementedException();
