362a66533307140166a675db571a3d4197af8c56
Cath20201 <78859065+Cath20201@users.noreply.github.com>
2023-02-26 19:10:00 +0100

    delopgave 7 done

--- ForbedrelseITS3EksamenNunit/TestFixedPricePayment.cs
+++ ForbedrelseITS3EksamenNunit/TestFixedPricePayment.cs
@@ -4,21 +4,37 @@ using NUnit.Framework;
 
 namespace ForbedrelseITS3EksamenNunit
 {
-    public class Tests
+    public class TestFixedPricePayment
     {
-        private MeterDataSample m_sample;
+        private MeterDataSample m_sample, m2_sample, m3_sample;
         private FixedPricePayment uut;
         [SetUp]
         public void Setup()
         {
-            m_sample = new MeterDataSample(1, Convert.ToDateTime("00:00:00"), 0.57);
+            m_sample = new MeterDataSample(1, Convert.ToDateTime("00:00:00"), 0.4);
+            m2_sample = new MeterDataSample(1, Convert.ToDateTime("01:30:00"), 0.24);
+            m3_sample = new MeterDataSample(1, Convert.ToDateTime("06:00:00"), 0.05);
             uut = new FixedPricePayment();
         }
 
-        [TestCase(2.041)]
-        public void Test_PriceBillForFixedPayment(double expectedResult)
+        [TestCase(1.432)]
+        public void Test_PriceBillForFixedPayment_m_sample(double expectedResult)
         {
             double price = uut.GetPriceBill(m_sample);
+            Assert.That(price, Is.EqualTo(expectedResult).Within(0.001));
+        }
+
+        [TestCase(0.859)]
+        public void Test_PriceBillForFixedPayment_m2_sample(double expectedResult)
+        {
+            double price = uut.GetPriceBill(m2_sample);
+            Assert.That(price, Is.EqualTo(expectedResult).Within(0.001));
+        }
+
+        [TestCase(0.179)]
+        public void Test_PriceBillForFixedPayment_m3_sample(double expectedResult)
+        {
+            double price = uut.GetPriceBill(m3_sample);
             Assert.That(price, Is.EqualTo(expectedResult).Within(0.001));
         }
     }
---
+++ ForbedrelseITS3EksamenNunit/TestFlexiblePricePayment.cs
@@ -0,0 +1,63 @@
+﻿using ForbedrelseITS3EksamenLibrary.GoF_Strategy;
+using ForbedrelseITS3EksamenLibrary;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForbedrelseITS3EksamenNunit
+{
+    public class TestFlexiblePricePayment
+    {
+        private MeterDataSample m_sample;
+        private MeterDataSample m2_sample;
+        private MeterDataSample m3_sample;
+        private MeterDataSample m4_sample;
+        private MeterDataSample m5_sample;
+        private FlexiblePricePayment uut;
+
+        [SetUp]
+        public void Setup()
+        {
+            m_sample = new MeterDataSample(1, Convert.ToDateTime("00:00:00"), 0.4);
+            m2_sample = new MeterDataSample(1, Convert.ToDateTime("06:00:00"), 0.4);
+            m3_sample = new MeterDataSample(1, Convert.ToDateTime("11:00:00"), 0.4);
+            m4_sample = new MeterDataSample(1, Convert.ToDateTime("20:00:00"), 0.4);
+            m5_sample = new MeterDataSample(1, Convert.ToDateTime("21:00:00"), 0.4);
+            uut = new FlexiblePricePayment();
+        }
+
+        [TestCase(0.58)]
+        public void Test_PriceBillForFlexiblePayment_m_sample(double expectedResult)
+        {
+            double price = uut.GetPriceBill(m_sample);
+            Assert.That(price, Is.EqualTo(expectedResult).Within(0.001));
+        }
+        [TestCase(1.32)]
+        public void Test_PriceBillForFlexiblePayment_m2_sample(double expectedResult)
+        {
+            double price = uut.GetPriceBill(m2_sample);
+            Assert.That(price, Is.EqualTo(expectedResult).Within(0.001));
+        }
+        [TestCase(0.76)]
+        public void Test_PriceBillForFlexiblePayment_m3_sample(double expectedResult)
+        {
+            double price = uut.GetPriceBill(m3_sample);
+            Assert.That(price, Is.EqualTo(expectedResult).Within(0.001));
+        }
+        [TestCase(1.74)]
+        public void Test_PriceBillForFlexiblePayment_m4_sample(double expectedResult)
+        {
+            double price = uut.GetPriceBill(m4_sample);
+            Assert.That(price, Is.EqualTo(expectedResult).Within(0.001));
+        }
+        [TestCase(0.48)]
+        public void Test_PriceBillForFlexiblePayment_m5_sample(double expectedResult)
+        {
+            double price = uut.GetPriceBill(m5_sample);
+            Assert.That(price, Is.EqualTo(expectedResult).Within(0.001));
+        }
+    }
+}
